aed58a1ce912f22afbd5b5b4f3ebaaa53effafe5
Eric Nguyen <19nguyene@tesdk12.net>
2018-07-30 08:38:47 -0400

    switch from underscore_case to camelCase

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function hash(plaintext, upper_case = false) {
+function hash(plaintext, upperCase = false) {
   if (plaintext === "") {
     return "";
   }
 
   let out = hex_sha256(plaintext.replace(new RegExp("\r", "g"), ""));
-  return upper_case ? out.toUpperCase() : out
+  return upperCase ? out.toUpperCase() : out
 }
 
-function hash_each_line(plaintext, upper_case = false) {
+function hashEachLine(plaintext, upperCase = false) {
   return plaintext.split(/\r?\n/).map(function(line) { 
-    return hash(line, upper_case); 
+    return hash(line, upperCase); 
   }).join("\n");
 }
 
-function generate_hash(plaintext, each_line = false, upper_case = false) {
-  return each_line ? hash_each_line(plaintext, upper_case) : hash(plaintext, upper_case);
+function generate_hash(plaintext, eachLine = false, upperCase = false) {
+  return eachLine ? hashEachLine(plaintext, upperCase) : hash(plaintext, upperCase);
 }
